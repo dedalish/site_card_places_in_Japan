e0b4c834fee00b06e9978c2a5a586f841afca071
dedalish <karatel.spamerov@gmail.com>
2022-05-26 17:07:30 +0700

    work

--- js/main.js
+++ js/main.js
@@ -1,10 +1,27 @@
 (function () {
     const header = document.querySelector('.header');
     window.onscroll = () => {
-        if (window.pageYOffset > 500) {
+        if (window.pageYOffset > 60) {
             header.classList.add('header_active');
         } else {
             header.classList.remove('header_active');
         }
     };
+}());
+
+//burger handler 
+(function () {
+    const burgerItem = document.querySelector('.burger');
+    const menu = document.querySelector('.header__nav');
+    const menuCloseItem = document.querySelector('.header__nav__close__line');
+
+    burgerItem.addEventListener('click', () => {
+        menu.classList.add('header__nav__active');
+
+    });
+
+    menuCloseItem.addEventListener('click', () => {
+        menu.classList.remove('header__nav__active');
+    });
+
 }());
